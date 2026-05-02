eaa7ad3fce88a8204c743d3a7fc3e9e9be1394c3
planethouki <26595148+planethouki@users.noreply.github.com>
2026-05-02 10:10:28 +0900

    ギャラリーページのタイトルを「Latest」から「Gallery」に変更

--- src/App.tsx
+++ src/App.tsx
@@ -107,7 +107,7 @@ function App() {
         <Route
           path="/"
           element={
-            <GalleryPage images={sortedImages} loadState={loadState} title="Latest" />
+            <GalleryPage images={sortedImages} loadState={loadState} title="Gallery" />
           }
         />
         <Route
